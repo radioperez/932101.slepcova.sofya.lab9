50a6eb0bf74052da29e43affad7e8de538134483
Slepcova <brostikslavik@yandex.com>
2023-10-28 12:02:44 +0700

    добавлены ограничения на точки и операторы

--- lab9.js
+++ lab9.js
@@ -1,7 +1,8 @@
 const display = document.getElementById("display").children[0];
+display.value = "";
+var hasPoint = false;
 
 const literalButtons = document.getElementsByClassName("lit");
-
 for (let i=0; i<literalButtons.length; i++) {
 	literalButtons[i].addEventListener('click', function(){
 		let num = this.innerHTML;
@@ -10,7 +11,32 @@ for (let i=0; i<literalButtons.length; i++) {
 	});
 }
 
-const controlButtons = document.getElementsByClassName("control");
+const pointButton = document.getElementById("point");
+pointButton.addEventListener('click', function() {
+	if (hasPoint == false) {
+		display.value += this.innerHTML;
+		hasPoint = true;
+	}
+	else {
+		hasPoint = true;
+	}
+});
+
+const operatorButtons = document.getElementsByClassName("operation");
+for (let i = 0; i<operatorButtons.length; i++) {
+	operatorButtons[i].addEventListener('click', function() {
+		if (display.value.slice(-1) == '+' || display.value.slice(-1) == '-' || display.value.slice(-1) == '*' || display.value.slice(-1) == '/') {
+			display.value = display.value.slice(0,-1);
+			display.value += this.innerHTML;
+		}
+		else {
+			let num = this.innerHTML;
+			display.value += num;
+			hasPoint = false;
+		}
+	});
+}
+
 const clearButton = document.getElementById("clear");
 
 clearButton.addEventListener('click', function() {
@@ -22,6 +48,7 @@ const deleteButton = document.getElementById("delete");
 deleteButton.addEventListener('click', function() {
 	if (display.length < 2) display.value = "0";
 	else {
+		if (display.value.slice(-1) == '.') hasPoint = false;
 		display.value = display.value.slice(0,-1);
 	}
 });
